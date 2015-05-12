99410e881b8bdf5d0826fd3a52f2bf07d15a9e41
Tim Flood <tpflood@att.net>
2015-05-12 15:19:49 -0700

    Check in working code with intermediate gen output
    Final program is supposed to output last generation.  This version
    produces final output, but still has intermediate generation display
    code.

--- Assignment-5/Assignment-5/Assignment-5.cpp
+++ Assignment-5/Assignment-5/Assignment-5.cpp
@@ -8,28 +8,75 @@
 #include <fstream>
 using namespace std;
 
-void DisplayGrid(int Grid[20][20], int MaxRow, int MaxColumn, bool OriginalGrid, int Gen)
+// Constants
+const int MAXROW = 20;
+const int MAXCOLUMN = 20;
+const int MAXGEN = 5;
+
+void DisplayGrid(int Grid[MAXROW][MAXCOLUMN], bool OriginalGrid, int Gen);
+
+void DisplayFinalGrid(int Grid[MAXROW][MAXCOLUMN], int Gen);
+
+bool InitializeGridFromFile(int Grid[MAXROW][MAXCOLUMN]);
+
+int CountLiveNeighbors(int Grid[MAXROW][MAXCOLUMN], int Row, int Column);
+
+void main()
+{
+	int Row, Column, Gen, NeighborLifeCount;
+	int Grid[MAXROW][MAXCOLUMN] = { 0 };
+
+	if (InitializeGridFromFile(Grid))
+	{
+		//	DisplayGrid(Grid, true, -1);
+
+		for (Gen = 0; Gen < MAXGEN; Gen++)
+		{
+			int NextGenGrid[MAXROW][MAXCOLUMN] = { 0 };
+
+			for (Row = 0; Row < MAXROW; Row++)
+			{
+				for (Column = 0; Column < MAXCOLUMN; Column++)
+				{
+					NeighborLifeCount = CountLiveNeighbors(Grid, Row, Column);
+
+					if (!Grid[Row][Column])
+					{
+						if (NeighborLifeCount == 3)
+						{
+							NextGenGrid[Row][Column]++;
+						}
+					}
+					else if ((NeighborLifeCount == 2) || (NeighborLifeCount == 3))
+					{
+						NextGenGrid[Row][Column]++;
+					}
+				}
+			}
+			for (Row = 0; Row < MAXROW; Row++)
+				for (Column = 0; Column < MAXCOLUMN; Column++)
+					Grid[Row][Column] = NextGenGrid[Row][Column];
+
+			//		DisplayGrid(Grid, false, Gen);
+		}
+		DisplayFinalGrid(Grid, Gen);
+	}
+	
+	system("PAUSE");
+}
+
+void DisplayGrid(int Grid[MAXROW][MAXCOLUMN], bool OriginalGrid, int Gen)
 {
 	int Row, Column;
-	int AliveInRow[20];
-	int AliveInColumn[20];
+	int AliveInRow[MAXROW] = { 0 };
+	int AliveInColumn[MAXCOLUMN] = { 0 };
 	int TotalAlive = 0;
 
-	for (Row = 0; Row < MaxRow; Row++)
-	{
-		AliveInRow[Row] = 0;
-	}
-
-	for (Column = 0; Column < MaxColumn; Column++)
-	{
-		AliveInColumn[Column] = 0;
-	}
-
-	cout << "  01234567890123456789" << endl;
-	for (Row = 0; Row < MaxRow; Row++)
+	cout << endl << "  01234567890123456789" << endl;
+	for (Row = 0; Row < MAXROW; Row++)
 	{
 		std::cout << std::setw(2) << Row;
-		for (Column = 0; Column < MaxColumn; Column++)
+		for (Column = 0; Column < MAXCOLUMN; Column++)
 		{
 			if (Grid[Row][Column])
 			{
@@ -49,7 +96,7 @@ void DisplayGrid(int Grid[20][20], int MaxRow, int MaxColumn, bool OriginalGrid,
 	if (OriginalGrid)
 	{
 		cout << "(ORIGINAL GRID) ";
-	} 
+	}
 	else
 	{
 		cout << endl << "The grid after " << (Gen + 1) << " generations have passed." << endl;
@@ -58,101 +105,95 @@ void DisplayGrid(int Grid[20][20], int MaxRow, int MaxColumn, bool OriginalGrid,
 	cout << "GAME OF LIFE STATISTICS" << endl << endl;
 	cout << "Total alive in row 10 = " << AliveInRow[10] << endl;
 	cout << "Total alive in col 10 = " << AliveInColumn[10] << endl;
-	cout << "Total dead in row 16 = " << (MaxRow - AliveInRow[16]) << endl;
-	cout << "Total dead in col 1 = " << (MaxColumn - AliveInColumn[1]) << endl;
+	cout << "Total dead in row 16 = " << (MAXROW - AliveInRow[16]) << endl;
+	cout << "Total dead in col 1 = " << (MAXCOLUMN - AliveInColumn[1]) << endl;
 	cout << "Total alive = " << TotalAlive << endl;
-	cout << "Total dead = " << ((MaxRow * MaxColumn) - TotalAlive) << endl << endl;
-	
+	cout << "Total dead = " << ((MAXROW * MAXCOLUMN) - TotalAlive) << endl;
+
 }
 
-void main(int argc, _TCHAR* argv[])
+void DisplayFinalGrid(int Grid[MAXROW][MAXCOLUMN], int Gen)
 {
-	const int MaxRow = 20;
-	const int MaxColumn = 20;
-	const int MaxGen = 8;
-	bool Temp;
-	int Row, Column, Gen, i, j, x, y, NeighborLifeCount;
-	int CellType;
+	int Row, Column;
+	int AliveInRow[MAXROW] = { 0 };
+	int AliveInColumn[MAXCOLUMN] = { 0 };
+	int TotalAlive = 0;
+
+	cout << endl << "  01234567890123456789" << endl;
+	for (Row = 0; Row < MAXROW; Row++)
+	{
+		std::cout << std::setw(2) << Row;
+		for (Column = 0; Column < MAXCOLUMN; Column++)
+		{
+			if (Grid[Row][Column])
+			{
+				cout << "*";
+				AliveInRow[Row]++;
+				AliveInColumn[Column]++;
+				TotalAlive++;
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+
+	cout << "GAME OF LIFE STATS ("<< Gen << "th Generation)...check figures listed below.." << endl;
+	cout << "Total alive in row 10 = " << AliveInRow[10] << endl;
+	cout << "Total alive in col 10 = " << AliveInColumn[10] << endl;
+	cout << "Total dead in row 16 = " << (MAXROW - AliveInRow[16]) << endl;
+	cout << "Total dead in col 1 = " << (MAXCOLUMN - AliveInColumn[1]) << endl;
+	cout << "Total alive = " << TotalAlive << endl;
+	cout << "Total dead = " << ((MAXROW * MAXCOLUMN) - TotalAlive) << endl;
+
+}
+
+bool InitializeGridFromFile(int Grid[MAXROW][MAXCOLUMN])
+{
 	ifstream Bacteria;
-	int Grid[MaxRow][MaxColumn];
-	int NextGenGrid[MaxRow][MaxColumn];
-
-	for (Row = 0; Row < MaxRow; Row++)
-		for (Column = 0; Column < MaxColumn; Column++)
-			Grid[Row][Column] = 0;
-
-			
+	int Row, Column;
+	bool Success;
 
 	Bacteria.open("bacteria.txt");
 	if (!Bacteria)
 	{
 		cout << "File not found" << endl;
+		Success = false;
 	}
 	else
 	{
+		Success = true;
 		while (!Bacteria.eof())
 		{
 			Bacteria >> Row >> Column;
-			if ((Row < MaxRow) && (Column < MaxColumn))
+			if ((Row < MAXROW) && (Column < MAXCOLUMN))
 			{
 				Grid[Row][Column] = 1;
-			}			
+			}
 		}
 	}
-	
-	DisplayGrid(Grid, MaxRow, MaxColumn, true, -1);
-
-	for (Gen = 0; Gen < MaxGen; Gen++)
-	{
-		for (Row = 0; Row < MaxRow; Row++)
-			for (Column = 0; Column < MaxColumn; Column++)
-				NextGenGrid[Row][Column] = 0;
-
-		for (Row = 0; Row < MaxRow; Row++)
-		{
-			for (Column = 0; Column < MaxColumn; Column++)
-			{
-				Temp = false;
-				if (Row == 2 && Column == 19)
-				{
-					Temp = true;
-				}
-				NeighborLifeCount = 0;
-				for (i = -1; i < 2; i++)
-				{
-					for (j = -1; j < 2; j++)
-					{
-						x = Row + i;
-						y = Column + j;
-						if (Temp)
-						{
-							Temp = true;
-						}
-						if ((i != 0 || j != 0) && (x >= 0) && (x < MaxRow) && (y >= 0) && (y < MaxColumn) && Grid[x][y])
-						{
-							NeighborLifeCount++;
-						}
-					}
-				}
-				if (!Grid[Row][Column])
-				{
-					if (NeighborLifeCount == 3)
-					{
-						NextGenGrid[Row][Column]++;
-					}
-				}
-				else if ((NeighborLifeCount == 2) || (NeighborLifeCount == 3))
-				{
-					NextGenGrid[Row][Column]++;
-				}
-			}			
-		}
-		for (Row = 0; Row < MaxRow; Row++)
-			for (Column = 0; Column < MaxColumn; Column++)
-				Grid[Row][Column] = NextGenGrid[Row][Column];
-
-		DisplayGrid(Grid, MaxRow, MaxColumn, false, Gen);
-		system("PAUSE");
-	}
+	return (Success);
 }
 
+int CountLiveNeighbors(int Grid[MAXROW][MAXCOLUMN], int Row, int Column)
+{
+	int i, j, x, y;
+	int NeighborLifeCount;
+
+	NeighborLifeCount = 0;
+	for (i = -1; i < 2; i++)
+	{
+		for (j = -1; j < 2; j++)
+		{
+			x = Row + i;
+			y = Column + j;
+			if ((i != 0 || j != 0) && (x >= 0) && (x < MAXROW) && (y >= 0) && (y < MAXCOLUMN) && Grid[x][y])
+			{
+				NeighborLifeCount++;
+			}
+		}
+	}
+	return(NeighborLifeCount);
+}
